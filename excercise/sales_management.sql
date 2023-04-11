871e4c11d917561a8a80af28b49ac2f46f5367bd
PhapKhuong <phapkhuong@gmail.com>
2023-04-12 03:14:23 +0700

    Truy vấn dữ liệu với CSDL quanlysinhvien

--- excercise/sales_management.sql
+++ excercise/sales_management.sql
@@ -1,92 +1,56 @@
 CREATE DATABASE sales_management;
 USE sales_management;
 
-CREATE TABLE san_pham
-	(
-		MaSP INT PRIMARY KEY AUTO_INCREMENT,
-        TenSP VARCHAR(50),
-        MaLoaiSP INT,
-        FOREIGN KEY (MaLoaiSP) REFERENCES loai_san_pham(MaLoaiSP),
-        SoLuongKho INT,
-        GiaBan INT,
-        ChiTiet TEXT
-    );
+CREATE TABLE san_pham (
+	MaSP INT PRIMARY KEY AUTO_INCREMENT,
+   TenSP VARCHAR(50),
+   MaLoaiSP INT,
+   FOREIGN KEY (MaLoaiSP) REFERENCES loai_san_pham(MaLoaiSP),
+   SoLuongKho INT,
+   GiaBan INT,
+   ChiTiet TEXT
+);
     
-CREATE TABLE loai_san_pham
-	(
-		MaLoaiSP INT PRIMARY KEY AUTO_INCREMENT,
-        TenLoaiSP VARCHAR(50)
-    );
+CREATE TABLE loai_san_pham (
+	MaLoaiSP INT PRIMARY KEY AUTO_INCREMENT,
+   TenLoaiSP VARCHAR(50)
+);
     
-CREATE TABLE khach_hang
-	(
-		MaKH INT PRIMARY KEY AUTO_INCREMENT,
-        TenKH VARCHAR (50)
-    );
+CREATE TABLE khach_hang (
+	MaKH INT PRIMARY KEY AUTO_INCREMENT,
+   TenKH VARCHAR (50)
+);
     
-CREATE TABLE so_dien_thoai
-	(
-		SDT VARCHAR(12),
-        MaKH INT,
-        FOREIGN KEY (MaKH) REFERENCES khach_hang (MaKH)
-    );
+CREATE TABLE so_dien_thoai (
+	SDT VARCHAR(12),
+   MaKH INT,
+   FOREIGN KEY (MaKH) REFERENCES khach_hang (MaKH)
+);
     
-CREATE TABLE dia_chi
-	(
-		MaDC INT PRIMARY KEY AUTO_INCREMENT,
-        MaKH INT,
-        FOREIGN KEY (MaKH) REFERENCES khach_hang (MaKH)
-    );
+ALTER TABLE khach_hang
+ADD dia_chi TEXT; 
     
-CREATE TABLE tinh
-	(
-		MaTinh INT PRIMARY KEY AUTO_INCREMENT,
-        TenTinh VARCHAR(50),
-        MaDC INT,
-        FOREIGN KEY (MaDC) REFERENCES dia_chi (MaDC)
-    );
+ALTER TABLE khach_hang    
+CHANGE dia_chi DiaChi TEXT;
     
-CREATE TABLE quan
-	(
-		MaQuan INT PRIMARY KEY AUTO_INCREMENT,
-        TenQuan VARCHAR(50),
-        MaDC INT,
-        FOREIGN KEY (MaDC) REFERENCES dia_chi (MaDC)
-    );
+CREATE TABLE dat_hang (
+	MaDH INT PRIMARY KEY AUTO_INCREMENT,
+	MaKH INT,
+   MaSP INT,
+   FOREIGN KEY (MaKH) REFERENCES khach_hang (MaKH),
+   FOREIGN KEY (MaSP) REFERENCES san_pham (MaSP),
+   SoLuongDH INT
+);
     
-CREATE TABLE phuong
-	(
-		MaPhuong INT PRIMARY KEY AUTO_INCREMENT,
-        TenPhuong VARCHAR(50),
-        MaDC INT,
-        FOREIGN KEY (MaDC) REFERENCES dia_chi (MaDC)
-    );
+ALTER TABLE dat_hang
+ADD TrangThai ENUM('Đã giao hàng','Đang giao hàng');
     
-CREATE TABLE dat_hang
-	(
-		MaDH INT PRIMARY KEY AUTO_INCREMENT,
-		MaKH INT,
-        MaSP INT,
-        FOREIGN KEY (MaKH) REFERENCES khach_hang (MaKH),
-        FOREIGN KEY (MaSP) REFERENCES san_pham (MaSP),
-        SoLuongDH INT
-    );
-    
-CREATE TABLE danh_gia
-	(
-		MaDG INT PRIMARY KEY AUTO_INCREMENT,
-		MaKH INT,
-        MaSP INT,
-        FOREIGN KEY (MaKH) REFERENCES khach_hang (MaKH),
-        FOREIGN KEY (MaSP) REFERENCES san_pham (MaSP),
-        NoiDung TEXT,
-        Sao VARCHAR(10)
-    );
-    
-CREATE TABLE trang_thai
-	(
-		TrangThai BIT,
-        MaDH INT,
-        FOREIGN KEY (MaDH) REFERENCES dat_hang (MaDH),
-        CONSTRAINT PRIMARY KEY (TrangThai, MaDH)
-    );
+CREATE TABLE danh_gia (
+	MaDG INT PRIMARY KEY AUTO_INCREMENT,
+	MaKH INT,
+   MaSP INT,
+   FOREIGN KEY (MaKH) REFERENCES khach_hang (MaKH),
+   FOREIGN KEY (MaSP) REFERENCES san_pham (MaSP),
+   NoiDung TEXT,
+   Sao VARCHAR(10)
+);
--- excercise/student_management.sql
+++ excercise/student_management.sql
@@ -1,32 +1,29 @@
 CREATE DATABASE student_management;
 USE student_management;
 
-CREATE TABLE student
-	(
-		id INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(45),
-        age INT,
-        country VARCHAR(45)
-    );
+CREATE TABLE student (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+   name VARCHAR(45),
+   age INT,
+   country VARCHAR(45)
+);
     
 ALTER TABLE student
 CHANGE name studnet_name VARCHAR(45),
 CHANGE id student_id INT,
 CHANGE age student_age INT;
 
-CREATE TABLE class
-	(
-		class_id INT PRIMARY KEY AUTO_INCREMENT,
-        class_name VARCHAR(10)
-    );
+CREATE TABLE class (
+	class_id INT PRIMARY KEY AUTO_INCREMENT,
+   class_name VARCHAR(10)
+);
     
-CREATE TABLE teacher
-	(
-		teacher_id INT PRIMARY KEY AUTO_INCREMENT,
-        teacher_name VARCHAR(45),
-        teacher_age INT,
-        country VARCHAR(45)
-    );
+CREATE TABLE teacher (
+	teacher_id INT PRIMARY KEY AUTO_INCREMENT,
+   teacher_name VARCHAR(45),
+   teacher_age INT,
+   country VARCHAR(45)
+);
 
 
 
